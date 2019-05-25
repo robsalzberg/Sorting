eebfd31ae2c5233e8d25f8b299dc67ea8ea41764
Rob Salzberg <robsalzberg@gmail.com>
2019-05-25 12:55:18 -0400

    Reformatting and implement recursive sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -19,13 +19,13 @@ def selection_sort(arr):
 def bubble_sort(arr):
     swapped = True
     while swapped:
-            swapped = False
-            for i in range(len(arr)):
-                    for j in range(0, len(arr) - i - 1):
-                        if arr[j] > arr[j+1]:
-                            arr[j], arr[j+1] = arr[j+1], arr[j]
-                            swapped = True
-            return arr
+        swapped = False
+        for i in range(len(arr)):
+            for j in range(0, len(arr) - i - 1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+        return arr
 
 
 # STRETCH: implement the Count Sort function below
